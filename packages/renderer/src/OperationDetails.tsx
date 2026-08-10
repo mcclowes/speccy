d8b96230655c3be670e219bf5c79a1bee6492794
Max Clayton Clowes <contact@mcclowes.com>
2026-08-10 09:28:57 +0100

    Restore unrelated renderer changes

--- packages/renderer/src/OperationDetails.tsx
+++ packages/renderer/src/OperationDetails.tsx
@@ -22,8 +22,7 @@ import { DisclosureChevron, HTTP_METHOD_LABELS, httpMethodLabel } from './Design
 import { EyeIcon } from './EyeIcon';
 import { Markdown } from './Markdown';
 import { RequestSample } from './RequestSample';
-import { ResponseDetails } from './ResourceDetails';
-import { SchemaView } from './SchemaView';
+import { ParameterDetails, ResponseDetails } from './ResourceDetails';
 import { SendIcon } from './SendIcon';
 import { useLocalState } from './useLocalState';
 
@@ -103,20 +102,8 @@ const DEFAULT_VISIBLE_PARAMETERS = 5;
 const MIN_COLLAPSIBLE_OPTIONAL_PARAMETERS = 3;
 
 function ParameterCard({ location, parameter, index }: { location: string; parameter: Parameter; index: number }) {
-  const example = parameter.example !== undefined ? parameter.example : parameter.schema?.example;
-  const schema = {
-    ...parameter.schema,
-    description: parameter.description ?? parameter.schema?.description,
-  };
-
   return <div className="sp-endpoint-parameter" key={`${location}-${parameter.name}-${index}`}>
-    <SchemaView
-      name={parameter.name ?? 'unnamed'}
-      schema={schema}
-      required={parameter.required}
-      summaryOnly
-      exampleValue={example}
-    />
+    <ParameterDetails parameter={parameter} summaryOnly />
   </div>;
 }
 
